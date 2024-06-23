4497ec0ff1799434f9446b0f46e632d1576cc97c
m-triple-m <triplem656@gmail.com>
2024-06-23 14:35:14 +0530

    create operation

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 const port = 5000;
 
 // middleware
+app.use(express.json());
+
 app.use('/user', UserRouter);
 
 // endpoint or route
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -3,8 +3,16 @@ const Model = require('../models/userModel');
 
 const router = express.Router();
 
-router.get('/add', (req, res) => {
-    res.send('response from user add');
+router.post('/add', (req, res) => {
+    console.log(req.body);
+
+    new Model(req.body).save()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 // getall
