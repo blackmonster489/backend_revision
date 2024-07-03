ca083b3b11a9fedaf858390c5bc6f74198986289
blackmonster489 <vshagun489@gmail.com>
2024-07-03 21:09:48 +0530

    hhjjfg

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const UserRouter = require('./routers/userRouter');
 const feedRouter = require('./routers/feedRouter');
+const productRouter = require('./routers/productRouter');
+const cors = require('cors');
 
 // initialize express
 const app = express();
@@ -9,9 +11,14 @@ const app = express();
 const port = 5000;
 
 // middleware
+
+app.use(cors({
+    origin : 'http://localhost:3000'
+}))
 app.use(express.json());
 app.use('/user', UserRouter);
 app.use('/feed', feedRouter);
+app.use('/product', productRouter);
 
 // endpoint or route
 app.get('/', (req, res) => {
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Model = require('../models/feedModel');
+const Model = require('../models/productModel');
 
 const router = express.Router();
 
